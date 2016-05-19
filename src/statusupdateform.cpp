3b830f2e076ef6e9ec5cd61c817da07972ca2d58
Taha Ibrahim Bayram <lacriment@gmail.com>
2016-05-19 17:20:39 +0300

    StatusUpdateForm updated

--- src/statusupdateform.cpp
+++ src/statusupdateform.cpp
@@ -21,7 +21,27 @@ void StatusUpdateForm::on_btn_find_clicked()
     Shipment *s = new Shipment();
     int trackingCode = ui->txt_trackingCode->text().toInt();
     s = Connection::getConnection()->getShipment(trackingCode);
-    ui->lbl_lblSender->setText(s->getSendingCustomer()->getName());
-    ui->lbl_lblReceiver->setText(s->getReceivingCustomer()->getName());
-    ui->lbl_status->setText(s->getStatus());
+    if (s->getAmount()){
+        ui->lbl_sender->setText(s->getSendingCustomer()->getName());
+        ui->lbl_receiver->setText(s->getReceivingCustomer()->getName());
+        ui->lbl_status->setText(s->getStatus());
+        ui->lbl_done->setText("Gönderi bulundu!");}
+    else {
+        ui->lbl_done->setText("Gönderi bulunamadı!");
+        ui->lbl_sender->setText("");
+        ui->lbl_receiver->setText("");
+        return;
+    }
+    delete s;
+}
+
+void StatusUpdateForm::on_pushButton_clicked()
+{
+    Shipment *s = new Shipment();
+    int trackingCode = ui->txt_trackingCode->text().toInt();
+    s = Connection::getConnection()->getShipment(trackingCode);
+    s->setStatus(ui->cb_status->currentText());
+    Connection::getConnection()->updateShipment(s);
+    ui->lbl_done->setText("Başarılı!");
+    delete s;
 }
--- src/statusupdateform.hpp
+++ src/statusupdateform.hpp
@@ -18,6 +18,8 @@ public:
 private slots:
     void on_btn_find_clicked();
 
+    void on_pushButton_clicked();
+
 private:
     Ui::StatusUpdateForm *ui;
 };
